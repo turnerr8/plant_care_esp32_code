4a0575b679072de85b9154dc2f59a7156dd059dd
ryan <ryan@rturner.dev>
2026-07-31 12:44:12 -0700

    added mac address to chip

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,22 @@
 #ifndef MOISTURE_MIN
 #define MOISTURE_MIN 1700
 #endif
+#ifndef CHIP_ID
+#define CHIP_ID ESP.getEfuseMac()
+#endif
 
 
 
 void setup() {
   Serial.begin(115200);
-
   delay(1000);
+  Serial.printf("Chip ID: %04X%08X\n", (uint16_t)(CHIP_ID >> 32), (uint32_t)CHIP_ID);
   Serial.println("Starting up moisture sensor...");
 }
 
 void loop() {
   float val = analogRead(MOISTURE_SENSOR);
-  Serial.print("Moisture sensor value: ");
-  Serial.print((MOISTURE_MAX - val) / (MOISTURE_MAX - MOISTURE_MIN) * 100);
-  Serial.println("%");
+  printf("moisture sensor value: %f%%\n", (MOISTURE_MAX - val) / (MOISTURE_MAX - MOISTURE_MIN) * 100);
   delay(1000);
 }
 
