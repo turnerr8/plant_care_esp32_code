5ce27b02cb52441f75751b1ddf650051fb3baeed
ryan <ryan@rturner.dev>
2026-08-01 16:09:52 -0700

    all data is read correctly and placed in SensorReading struct. This will then be jsonified and sent over api.

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,32 @@ int MOISTURE_SENSOR = 34;
 int MOISTURE_MAX = 3500;
 int MOISTURE_MIN = 1700;
 
+//struct that will hold all reading information
+struct SensorReading {
+  uint64_t chipId;
+  float moistureReading;
+  String timeStamp;
+};
+
+SensorReading reading;
+
 //get mac id for this esp32 chip to use as unique identifier
 uint64_t CHIP_ID = ESP.getEfuseMac();
 
 //ntp pool vars
 const char* ntpServer = "pool.ntp.org";
+
+//functions
+
+//print all readings
+void printReadings(const SensorReading& reading){
+  Serial.printf("Chip ID: %04X%08X\n",
+                  (uint16_t)(reading.chipId >> 32),
+                  (uint32_t)reading.chipId);
+  Serial.printf("Moisture level: %.2f%%\n", reading.moistureReading);
+  Serial.printf("Timestamp: %s\n", reading.timeStamp.c_str());
+
+}
 
 //connects to wifi network using ssid and wifi password defined in secrets.h
 void setupWifi(){
@@ -30,48 +51,26 @@ void setupWifi(){
 }
 
 //DEV: used to print local time for timeinfo structure testing
-void printLocalTime() {
+void getLocalTime() {
   struct tm timeinfo;
   if(!getLocalTime(&timeinfo)){
     Serial.println("Failed to get time info.");
     return;
   }
 
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-  Serial.print("Day of week: ");
-  Serial.println(&timeinfo, "%A");
-  Serial.print("Month: ");
-  Serial.println(&timeinfo, "%B");
-  Serial.print("Day of Month: ");
-  Serial.println(&timeinfo, "%d");
-  Serial.print("Year: ");
-  Serial.println(&timeinfo, "%Y");
-  Serial.print("Hour: ");
-  Serial.println(&timeinfo, "%H");
-  Serial.print("Hour (12 hour format): ");
-  Serial.println(&timeinfo, "%I");
-  Serial.print("Minute: ");
-  Serial.println(&timeinfo, "%M");
-  Serial.print("Second: ");
-  Serial.println(&timeinfo, "%S");
-
-  Serial.println("Time variables");
-  char timeHour[3];
-  strftime(timeHour,3, "%H", &timeinfo);
-  Serial.println(timeHour);
-  char timeWeekDay[10];
-  strftime(timeWeekDay,10, "%A", &timeinfo);
-  Serial.println(timeWeekDay);
-  Serial.println();
-  
-  
+  char timestamp[30];
+  strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", &timeinfo);
+  reading.timeStamp = timestamp;
+  //Serial.println(timestamp);
 }
 
 //reads analog input from moisture sensor and normalizes it from 1-100 based on readings that i got
 //sometimes goes over 100 but only rarely when submerged
 void readSensor() {
   float val = analogRead(MOISTURE_SENSOR);
-  printf("moisture sensor value: %f%%\n", (MOISTURE_MAX - val) / (MOISTURE_MAX - MOISTURE_MIN) * 100);
+  val = (MOISTURE_MAX - val) / (MOISTURE_MAX - MOISTURE_MIN) * 100;
+  reading.moistureReading = val;
+  //printf("moisture sensor value: %f%%\n", val);
 }
 
 
@@ -87,12 +86,16 @@ void setup() {
   setenv("TZ", "PST8PDT,M3.2.0,M11.1.0", 1);
   tzset();
 
-  Serial.printf("Chip ID: %04X%08X\n", (uint16_t)(CHIP_ID >> 32), (uint32_t)CHIP_ID);
+  reading.chipId = CHIP_ID;
+  // Serial.printf("Chip ID: %04X%08X\n", (uint16_t)(CHIP_ID >> 32), (uint32_t)CHIP_ID);
   Serial.println("Starting up moisture sensor...");
 }
 
 void loop() {
-  printLocalTime();
+  getLocalTime();
+  readSensor();
+  printReadings(reading);
+
   delay(3000);
 }
 
